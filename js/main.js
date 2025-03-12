8101f521cf9b5a0a9166e0bf2642fa72e2016e3b
Mohammed hussein <mohammedhussein1225m@gmail.com>
2025-03-12 22:02:01 +0200

    fixed caching error

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register("/Dashboard/sw.js")
     .then(() => console.log("Service Worker Registered"))
     .catch((err) => console.log("Service Worker Failed:", err));
 }
